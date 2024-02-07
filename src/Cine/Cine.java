493c4308f8cedafa30ae01593d9814b348d0c621
PePoPiPu <alexsugimoto@outlook.com>
2024-02-07 23:43:08 +0100

    build(src): .gitignore added, Cine.java and Main.java added
    Attributes added to Cine.java

---
+++ src/Cine/Cine.java
@@ -0,0 +1,11 @@
+package Cine;
+
+import Peliculas.Peliculas;
+import Sala.Sala;
+
+public class Cine {
+    private int aforo = 320;
+    public static int numSalas = 14;
+    public Cine() {
+    }
+}
---
+++ src/Main/Main.java
@@ -0,0 +1,12 @@
+package Main;
+import Cine.Cine;
+import Sala.Sala;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+}
+
